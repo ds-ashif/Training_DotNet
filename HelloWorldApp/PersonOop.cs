45cc12307e99b2f7a9de4c83b880c7206f675a1a
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-01-13 15:13:45 +0530

    Day1+Day2+Day3_Fundalmentals

--- HelloWorldApp/PersonOop.cs
+++ HelloWorldApp/PersonOop.cs
@@ -10,7 +10,7 @@ namespace HelloWorldApp
     
     public class Person
     {
-        public int Id{get; set; }
+        public int Id{get; set;  }
         public int Age{get; set; }
         public string Name{get; set; }
 
