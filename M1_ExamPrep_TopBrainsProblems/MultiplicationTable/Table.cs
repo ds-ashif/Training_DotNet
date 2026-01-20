d6fdb54e161fb59607715b86a124eb63cf1f176e
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-01-20 21:55:59 +0530

    Proper comments added

--- M1_ExamPrep_TopBrainsProblems/MultiplicationTable/Table.cs
+++ M1_ExamPrep_TopBrainsProblems/MultiplicationTable/Table.cs
@@ -2,27 +2,49 @@ using System;
 
 namespace MultiplicationTable
 {
-    class Table{
-
-    public static void Main()
+    /// <summary>
+    /// Generates and displays the multiplication table of a given number
+    /// up to a specified limit.
+    /// </summary>
+    class Table
     {
-        Console.WriteLine("Enter a number to generate its multiplication table:");
-        int n=int.Parse(Console.ReadLine());
-        Console.WriteLine($"Enter a number upto which multiplication table of {n} is to be generated");
-        int upTo=int.Parse(Console.ReadLine());
+        #region Program Entry Point
 
-        int[] Table=new int[upTo];
-
-        for(int i = 1; i <= upTo; i++)
+        /// <summary>
+        /// Program entry point.
+        /// Reads input from the user and prints the multiplication table.
+        /// </summary>
+        public static void Main()
         {
-            Table[i-1]=n*i;
+            Console.WriteLine("Enter a number to generate its multiplication table:");
+            int n = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine(
+                $"Enter the number up to which the multiplication table of {n} is to be generated:"
+            );
+            int upTo = int.Parse(Console.ReadLine()!);
+
+            ///<summary>
+            /// Multiplication table array that stores result at each iteration
+            /// </summary>
+            
+            int[] table = new int[upTo];
+
+            for (int i = 1; i <= upTo; i++)
+            {
+                table[i - 1] = n * i;
+            }
+
+            Console.WriteLine(
+                $"Multiplication table of {n} up to {upTo} is:"
+            );
+
+            for (int i = 0; i < upTo; i++)
+            {
+                Console.Write($"{table[i]} ");
+            }
         }
 
-        Console.Write($"Multiplication table of {n} upto {upTo} is:");
-        for(int i = 0; i < upTo; i++)
-        {
-            Console.Write($"  {Table[i]} ");
-        }
-    }
+        #endregion
     }
 }
