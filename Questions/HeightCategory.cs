161c98e9e9f6d962558cb399138fc490986efe19
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-02-03 15:05:52 +0530

    commented

--- Questions/HeightCategory.cs
+++ Questions/HeightCategory.cs
@@ -2,21 +2,42 @@ using System;
 
 namespace Questions
 {
-    /// <summary>Classifies height into categories.</summary>
+    /// <summary>
+    /// Determines the height category of a person based on height in centimeters.
+    /// </summary>
     public class HeightCategory
     {
+        /// <summary>
+        /// Application entry point.
+        /// Reads height input and displays the category.
+        /// </summary>
         public static void Main()
         {
+            #region Input
+
+            // Read height in centimeters from user
+            Console.WriteLine("Enter height in centimeters:");
             int heightCm = int.Parse(Console.ReadLine());
 
+            #endregion
+
+            #region Height Classification Logic
+
+
             if (heightCm < 150)
-                Console.WriteLine("Dwarf");
-            else if (heightCm < 165)
+            {
+                Console.WriteLine("Short");
+            }
+            else if (heightCm < 180)
+            {
                 Console.WriteLine("Average");
-            else if (heightCm <= 190)
-                Console.WriteLine("Tall");
+            }
             else
-                Console.WriteLine("Abnormal");
+            {
+                Console.WriteLine("Tall");
+            }
+
+            #endregion
         }
     }
 }
