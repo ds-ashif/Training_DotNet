56d94bfd79aed3379e5295c7c79820b5fa4f7fc2
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-02-13 09:15:27 +0530

    practice stringtonumbers

---
+++ M1ClassroomPractice/StringandStringBuilderPractice/stringToNumber/question1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //take input string as a integer
+        Console.WriteLine("Enter string numbers ");
+        string num = Console.ReadLine();
+        
+        //check type with handelling error risk so use out keyword
+        if(int.TryParse(num, out int number))
+        {
+            Console.WriteLine("string number converted to int.");
+            
+        }
+        else
+        {
+            Console.WriteLine("Please enter a number in string");
+        }
+    }
+}
