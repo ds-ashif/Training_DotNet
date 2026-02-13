373b0f893c9994308e7fc8b75e30141b24fa6fe1
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-02-14 00:44:39 +0530

    Attendance-First Unique Entry added

---
+++ M1ClassroomPractice/Practice13Feb/Attendance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static List<int> CheckUniqueAttendance(List<int> scans)
+    {
+        HashSet<int> track = new HashSet<int>();
+        foreach(var item in scans)
+        {
+            track.Add(item);
+        }
+        return track.ToList();
+    }
+    static void Main()
+    {
+        var input = new List<int>{10, 20, 10, 30, 20, 40};
+        var result = CheckUniqueAttendance(input);
+        Console.WriteLine("First time punch: ");
+
+        foreach(var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
