c7a87a403634164a4ff93f1c73cd5f49a2877451
Ashif Ahamad <datascientist.ashif@gmail.com>
2026-02-14 00:31:21 +0530

    Ecommerce cart consolidation

---
+++ M1ClassroomPractice/Practice13Feb/Ecommerce/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static Dictionary<string,int> CountCartQuantity(List<(string sku, int qty)> scans)
+    {
+        Dictionary<string,int> skuQty = new Dictionary<string, int>();
+        
+        foreach(var entry in scans)
+        {
+            if(entry.Item2<=0) continue;
+
+            if (!skuQty.ContainsKey(entry.Item1))
+            {
+                
+                    skuQty[entry.Item1] = entry.Item2;
+                
+            }
+            else
+            {
+                skuQty[entry.Item1]+=entry.Item2;
+            }
+        }
+        return skuQty;
+    }
+    static void Main()
+    {
+        var input = new List<(string, int)> {("A101",2),("B205",1),("A101",3),("C111",-1)};
+        var result = CountCartQuantity(input);
+        foreach(var item in result)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
